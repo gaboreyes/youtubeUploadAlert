0e76a4b7427b5066da6949191d379e962102b38a
gabriel <greyesleon33@gmail.com>
2023-09-15 10:36:11 -0500

    refactor: change the reply logic for get videos to avoind getting interaction error

--- src/bot/commands/getVideos.ts
+++ src/bot/commands/getVideos.ts
@@ -5,9 +5,11 @@ import { YoutubeApi } from "../../app/YoutubeApi.ts";
 import { IYoutubeChannel } from "../../interfaces/interfaces.ts";
 
 const command = {
-  data: new SlashCommandBuilder().setName('get-videos').setDescription('Returns the latest video url for the used channel'),
+  data: new SlashCommandBuilder().setName('get-videos').setDescription('Returns the latest video for each watched channel'),
   async execute(interaction: any) {
     const hiddenResponse = process.env.EPHEMERAL_REPLY === 'true' ? true : false
+    await interaction.reply({ content: 'Fetching videos...', ephemeral: hiddenResponse })
+
     const channelList: string[] = []
     const databaseApi = DatabaseApi.getInstance()
     const watchedChannels: IYoutubeChannel[] = await databaseApi.getWatchedChannels()
@@ -18,11 +20,7 @@ const command = {
     const youtubeApi = YoutubeApi.getInstance()
     const latestVideos = await youtubeApi.getLatestVideosFlow(channelList)
     for (let index = 0; index < latestVideos.length; index++) {
-      if(index === 0){
-        await interaction.reply({ content: latestVideos[0].videoUrl, ephemeral: hiddenResponse })
-      } else {
-        await interaction.followUp({ content: latestVideos[index].videoUrl, ephemeral: hiddenResponse })
-      }
+      await interaction.followUp({ content: latestVideos[index].videoUrl, ephemeral: hiddenResponse })
     }
     databaseApi.insertVideoFlow(latestVideos)
   }
